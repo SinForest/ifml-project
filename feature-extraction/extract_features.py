7d716ae2b3b4b86e07ecd3ed80e3dfbe68ecc1da
Patrick Dammann <github@thesinforest.eu>
2018-04-02 23:14:18 +0200

    better feature-extraction++3

--- feature-extraction/extract_features.py
+++ feature-extraction/extract_features.py
@@ -21,8 +21,9 @@ class PosterSet(torch.utils.data.Dataset):
         im = Image.open(self.path + fname + ".jpg").convert("RGB")
         return im
 
-    def __init__(self, path, data, setname, debug=False):
+    def __init__(self, path, data, setname, gen_d, debug=False):
         self.path = path
+        self.gen_d = gen_d
         if setname == 'all':
             data['all'] = {}
             data['all']['ids'] = data['train']['ids'] + data['val']['ids'] + data['test']['ids']
@@ -38,24 +39,25 @@ class PosterSet(torch.utils.data.Dataset):
         self.preproc = torchvision.transforms.Compose([scale, torchvision.transforms.ToTensor(), norm])
 
     def __getitem__(self, index):
-        return self.preproc(self.X[index]), self.y[index]
+        return self.preproc(self.X[index]), self.frac_hot(self.y[index])
 
     def __len__(self):
         return len(self.X)
 
-def frac_hot(y, d, twoway=True):
-    num = int(len(d) / 2) if twoway else len(d)
-    a = np.zeros(num)
-    [d[x] for x in y]
-    a[y] = 1.0 / len(y)
-    return a
+    def frac_hot(self, y):
+        num = int(len(gen_d) / 2)
+        a = np.zeros(num)
+        y = [self.gen_d[x] for x in y]
+        a[y] = 1.0 / len(y)
+        return a
 
 p       = pickle.load(open(DATASET_PATH, 'rb'))
 gen_d   = pickle.load(open(GENRE_DICT_PATH, 'rb'))
 h5      = h5py.File("../sets/features_all.h5", 'a')
-dataset = PosterSet(POSTER_PATH, p, 'all')
+dataset = PosterSet(POSTER_PATH, p, 'all', gen_d)
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=1)
-h5.create_dataset("labels", data=np.stack([frac_hot(y, gen_d) for __, y in tqdm(dataset, desc="labels")]))
+
+h5.create_dataset("labels", data=np.concatenate([y for __, y in dataloader]))
 h5.create_dataset("ids", data=np.array([s.encode('utf8') for s in dataset.ids]))
 
 
