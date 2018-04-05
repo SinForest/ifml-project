09e655bb6fc71ca53fa4231b6e128b023d38313b
Thorsten <t.wuensche@stud.uni-heidelberg.de>
2018-04-05 15:10:35 +0200

    expiriments with other methods

--- kneighbors/simple_methods.py
+++ kneighbors/simple_methods.py
@@ -6,13 +6,15 @@ Created on Thu Apr  5 11:09:03 2018
 @author: twuensche
 """
 import sys
-from sklearn.datasets import make_multilabel_classification
-from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-import numpy as np
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+import pickle
+import os
+
 
 import h5py
 
@@ -22,20 +24,9 @@ feature_sets = ['alex_fc6', 'alex_fc7', 'vgg19bn_fc6', 'vgg19bn_fc6', \
                 'vgg19bn_fc7', 'res50_avg', 'dense161_last']
 
 def main():
-    mode, X_train, X_test, Y_train, Y_test = handle_user_input()
-    if mode == 'test':
-        model = KNeighborsClassifier(n_neighbors=n_neighbors)
-        print('begin training..')
-        model.fit(X_train, Y_train)
-        print('begin prediction..')
-        prediction = model.predict(X_test)
-        print('write report..')
-        report = classification_report(Y_test, prediction)
-        print(report)
-        accuracy = accuracy_score(Y_test, prediction)
-        print(accuracy)
-        
+    mode = handle_user_input()
     if mode == 'kneighbors':
+        X_train, X_test, Y_train, Y_test = load_movie_data('alex_fc6')
         model = KNeighborsClassifier(n_neighbors=n_neighbors)
         print('begin training..')
         model.fit(X_train, Y_train)
@@ -47,6 +38,7 @@ def main():
         accuracy = accuracy_score(Y_test, prediction)
         print(accuracy)
     if mode == 'rforest':
+        X_train, X_test, Y_train, Y_test = load_movie_data('alex_fc6')
         model = RandomForestClassifier()
         print('begin training..')
         model.fit(X_train, Y_train)
@@ -57,33 +49,48 @@ def main():
         print(report)
         accuracy = accuracy_score(Y_test, prediction)
         print(accuracy)
+    if mode == 'onevsrest':
+        X_train, X_test, Y_train, Y_test = load_movie_data('alex_fc6')
+        model = OneVsRestClassifier(SVC(kernel='linear'), n_jobs = -1)
+        print('begin training..')
+        model.fit(X_train[:100], Y_train[:100])
+        print('begin prediction..')
+        prediction = model.predict(X_test[:100])
+        accuracy = accuracy_score(Y_test, prediction)
+        print(accuracy)
+        write_model(model)
+    if mode == 'all':
+        for set in feature_sets:
+            X_train, X_test, Y_train, Y_test = load_movie_data(set)
+            model = RandomForestClassifier()
+            print('begin training..')
+            model.fit(X_train, Y_train)
+            print('begin prediction..')
+            prediction = model.predict(X_test)
+            print('write report..')
+            report = classification_report(Y_test, prediction)
+            print(report)
+            accuracy = accuracy_score(Y_test, prediction)
+            print(accuracy)
+
 
 def handle_user_input():
     inputs = sys.argv[1:]
     mode = ''
     if len(inputs) > 0:
-        if inputs[0] == 'test':
-            X_train, X_test, Y_train, Y_test = make_test_data()
-            mode = 'test'
-        elif inputs[0] == 'neighbors':
-            X_train, X_test, Y_train, Y_test = load_movie_data('alex_fc6')
+        if inputs[0] == 'neighbors':
             mode = 'kneighbors'
         elif inputs[0] == 'forest':
-            X_train, X_test, Y_train, Y_test = load_movie_data('alex_fc6')
             mode = 'rforest'
-         
+        elif inputs[0] == 'onevsrest':
+            mode = 'onevsrest'
+        elif inputs[0] == 'all':
+            mode = 'all'
     else:
         help()
         sys.exit()
-    return mode, X_train, X_test, Y_train, Y_test
+    return mode
 
-def make_test_data():
-    X, Y = make_multilabel_classification(n_classes=2, n_labels=1,
-                                      allow_unlabeled=False,
-                                      random_state=1)
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, 
-                                                    test_size=0.25)
-    return X_train, X_test, Y_train, Y_test
 
 def load_movie_data(feature_set):
     
@@ -111,6 +118,18 @@ def help():
     print('How to use the kneighbours script:')
     print('TODO')
     
+def write_model(trained_classifier):
+    with open('trained_classifier', 'wb') as fp:
+        pickle.dump(trained_classifier, fp)
+        print('model written to file')
+
+def read_model():
+    if os.path.exists('trained_classifier'):
+        with open ('trained_classifier', 'rb') as fp:
+            print('trained_classifier loaded')
+            return pickle.load(fp)
+    else:
+        return []
 
 
 
