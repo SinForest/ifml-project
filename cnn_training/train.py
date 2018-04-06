63bf58b22e41570ad130f02a48f4a0782da86c72
Patrick Dammann <github@thesinforest.eu>
2018-04-07 00:44:46 +0200

    Added ReduceLROnPlateauWithLog

--- cnn_training/train.py
+++ cnn_training/train.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 
 import os
 import random
+import numpy as np
 from hashlib import md5
 from tqdm import tqdm, trange
 import pickle
@@ -29,12 +30,22 @@ TERM = {'y'  : "\33[33m",
         'm'  : "\33[35m",
         'clr': "\33[m"}
 
+class ReduceLROnPlateauWithLog(torch.optim.lr_scheduler.ReduceLROnPlateau):
+    def __init__(self, *args, **kwargs):
+        super(ReduceLROnPlateauWithLog, self).__init__(*args, **kwargs)
+        self.logger = []
+
+    def _reduce_lr(self, epoch):
+        self.logger.append(epoch)
+        return super(ReduceLROnPlateauWithLog, self)._reduce_lr(epoch)
+
 def plot_losses(d, rnd):
     x, y = zip(*d["train"].items())
     X, Y = zip(*d["val"].items())
     plt.clf()
     plt.plot(x, y, "g-", label='training')
     plt.plot(X, Y, "b-", label='validation')
+    plt.scatter(np.array(X)[d["lr"]], np.array(Y)[d["lr"]], c='b', marker='v')
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.legend()
@@ -57,86 +68,91 @@ def hook(m, i, o):
     print("weight:", m.weight.data)
     print(TERM['clr'])
 
-# prepare data
-gen_d = pickle.load(open(GENRE_DICT_PATH, 'rb'))
-split = pickle.load(open(DATASET_PATH, 'rb'))
+if __name__ == "__main__":
 
-tr_set = PosterSet(POSTER_PATH, split, 'train', gen_d=gen_d, augment=True, debug=True)
-tr_load = DataLoader(tr_set, batch_size=32, shuffle=True, num_workers=6, drop_last=True)
+    # prepare data
+    gen_d = pickle.load(open(GENRE_DICT_PATH, 'rb'))
+    split = pickle.load(open(DATASET_PATH, 'rb'))
 
-va_set = PosterSet(POSTER_PATH, split, 'val', gen_d=gen_d, augment=False, debug=True)
-va_load = DataLoader(va_set, batch_size=32, shuffle=False, num_workers=6, drop_last=True)
+    tr_set = PosterSet(POSTER_PATH, split, 'train', gen_d=gen_d, augment=True, debug=True)
+    tr_load = DataLoader(tr_set, batch_size=32, shuffle=True, num_workers=6, drop_last=True)
 
-# prepare model and training utillity
-net  = DebugNetwork(tr_set[0][0].size()[1:], len(gen_d) // 2)
-if CUDA_ON:
-    net.cuda()
-l_fn = torch.nn.BCELoss(size_average=False)
-opti = torch.optim.Adam(net.parameters()) #TODO: better optim
+    va_set = PosterSet(POSTER_PATH, split, 'val', gen_d=gen_d, augment=False, debug=True)
+    va_load = DataLoader(va_set, batch_size=32, shuffle=False, num_workers=6, drop_last=True)
 
-"""
-#DEBUG!!:
-print("##################### IM HERE! #####################")
-for m in net.modules():
-    if isinstance(m, nn.BatchNorm2d):
-        m.register_forward_hook(hook)
-"""
-losses = {"train":{}, "val":{}}
-va_delay  = 1 #DEBUG!
-bg_proc = None
+    # prepare model and training utillity
+    net  = DebugNetwork(tr_set[0][0].size()[1:], len(gen_d) // 2)
+    l_fn = torch.nn.BCELoss(size_average=False)
+    opti = torch.optim.Adam(net.parameters())
+    sdlr = ReduceLROnPlateauWithLog(opti, 'min')
 
-# generate random new folder to store all snapshots
-rnd_token = None
-while not rnd_token or os.path.isdir(rnd_token):
-    rnd_token = "_" + md5(str(random.random()).encode()).hexdigest()[:10]
-os.mkdir(rnd_token)
+    if CUDA_ON:
+        net.cuda()
 
-print("=== Starting Training with Token [{}] ===".format(rnd_token))
+    """
+    #DEBUG!!:
+    print("##################### IM HERE! #####################")
+    for m in net.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.register_forward_hook(hook)
+    """
+    losses = {"train":{}, "val":{}}
+    va_delay  = 1 #DEBUG!
+    bg_proc = None
 
-for epoch in trange(1,101):
-    tr_err = 0
-    net.train() # puts model into 'train' mode (some layers behave differently)
-    for X, y in tr_load:         # iterate over training set (in mini-batches)
-        X, y = (Var(X), Var(y)) if CUDA_ON else (Var(X).cuda(), Var(y).cuda())
-        # wrap data into Variables (for back-prop) and maybe move to GPU
+    # generate random new folder to store all snapshots
+    rnd_token = None
+    while not rnd_token or os.path.isdir(rnd_token):
+        rnd_token = "_" + md5(str(random.random()).encode()).hexdigest()[:10]
+    os.mkdir(rnd_token)
+
+    print("=== Starting Training with Token [{}] ===".format(rnd_token))
+
+    for epoch in trange(1,101):
+        tr_err = 0
+        net.train() # puts model into 'train' mode (some layers behave differently)
+        for X, y in tr_load:         # iterate over training set (in mini-batches)
+            X, y = (Var(X), Var(y)) if CUDA_ON else (Var(X).cuda(), Var(y).cuda())
+            # wrap data into Variables (for back-prop) and maybe move to GPU
+            
+            #TRAINING-STEP:
+            opti.zero_grad()         # "reset" the optimizers gradients
+            pred = net(X)            # forward pass
+            loss = l_fn(pred, y)     # calculate loss
+            loss.backward()          # backward pass
+            opti.step(loss)          # update weights
+            
+            tr_err += loss.data[0]   # log training loss
+
+        #[end] X,y in dataloader
         
-        #TRAINING-STEP:
-        opti.zero_grad()         # "reset" the optimizers gradients
-        pred = net(X)            # forward pass
-        loss = l_fn(pred, y)     # calculate loss
-        loss.backward()          # backward pass
-        opti.step()              # update weights
-        
-        tr_err += loss.data[0]   # log training loss
+        losses["train"][epoch] = tr_err / len(tr_set) # average loss over epoch and save
+        tqdm.write("Epoch {} - loss: {:.5f}".format(epoch, losses["train"][epoch]))
 
-    #[end] X,y in dataloader
-    
-    losses["train"][epoch] = tr_err / len(tr_set) # average loss over epoch and save
-    tqdm.write("Epoch {} - loss: {:.5f}".format(epoch, losses["train"][epoch]))
+        if epoch % va_delay == 0:  # validate and save every <va_delay>th epoch
+            net.eval() # puts model into 'eval' mode (some layers behave differently)
+            if CUDA_ON:
+                va_sum = [l_fn(net(Var(X, volatile=True)).cuda(), Var(y).cuda()).data[0] for X,y in va_load] # val forward passes (GPU)
+            else:
+                va_sum = [l_fn(net(Var(X, volatile=True)), Var(y)).data[0] for X,y in va_load] # val forward passes (CPU)
+            print([net(Var(X, volatile=True)).data for X,y in va_load])
+            #print([(X, y) for X,y in va_load][:2])
+            losses["val"][epoch] = sum(va_sum) / len(va_set)
+            tqdm.write("  -->{}Validating - loss: {:.5f}{}".format(TERM['g'], losses["val"][epoch], TERM['clr']))
+            sdlr.step(["val"][epoch])
+            losses["lr"] = sdlr.logger
+            # plot loss in BG:
+            if bg_proc:
+                bg_proc.join(2)
+                bg_proc.terminate()
+            bg_proc = Process(target=plot_losses, args=(losses,rnd_token))
+            bg_proc.start()
 
-    if epoch % va_delay == 0:  # validate and save every <va_delay>th epoch
-        net.eval() # puts model into 'eval' mode (some layers behave differently)
-        if CUDA_ON:
-            va_sum = [l_fn(net(Var(X, volatile=True)).cuda(), Var(y).cuda()).data[0] for X,y in va_load] # val forward passes (GPU)
-        else:
-            va_sum = [l_fn(net(Var(X, volatile=True)), Var(y)).data[0] for X,y in va_load] # val forward passes (CPU)
-        print([net(Var(X, volatile=True)).data for X,y in va_load])
-        #print([(X, y) for X,y in va_load][:2])
-        losses["val"][epoch] = sum(va_sum) / len(va_set)
-        tqdm.write("  -->{}Validating - loss: {:.5f}{}".format(TERM['g'], losses["val"][epoch], TERM['clr']))
-
-        # plot loss in BG:
-        if bg_proc:
-            bg_proc.join(2)
-            bg_proc.terminate()
-        bg_proc = Process(target=plot_losses, args=(losses,rnd_token))
-        bg_proc.start()
-
-        # save model do disk
-        state = {'state_dict':net.state_dict(), 'opti':opti.state_dict(), 'epoch':epoch, 'losses':losses}
-        f_name = rnd_token + "/model_{:04d}_{:.0f}.nn".format(epoch, losses["val"][epoch]*10000)
-        torch.save(state, f_name)
-        tqdm.write("  -->{}saved model to {}{}".format(TERM['y'], f_name, TERM['clr']))
-        
-    #[end] epoch % va_delay == 0
-#[end] epoch in range(1,101)
+            # save model do disk
+            state = {'state_dict':net.state_dict(), 'opti':opti.state_dict(), 'epoch':epoch, 'losses':losses}
+            f_name = rnd_token + "/model_{:04d}_{:.0f}.nn".format(epoch, losses["val"][epoch]*10000)
+            torch.save(state, f_name)
+            tqdm.write("  -->{}saved model to {}{}".format(TERM['y'], f_name, TERM['clr']))
+            
+        #[end] epoch % va_delay == 0
+    #[end] epoch in range(1,101)
