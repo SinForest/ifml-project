9bb4ca7c22b8b4c9a742463fc945ce5da6e215a8
twuensche-uni-hd <t.wuensche@stud.uni-heidelberg.de>
2018-04-07 16:35:59 +0200

    run experiments

--- simple_approaches/one_vs_rest_qda.py
+++ simple_approaches/one_vs_rest_qda.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr  6 16:25:13 2018
+Created on Thu Apr  5 16:27:54 2018
 
 @author: twuensche
 """
 
 import simple_methods as util
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.multiclass import OneVsRestClassifier
-
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from tqdm import tqdm
 
 def main():
-   for data_set in tqdm(util.feature_sets):    
+    for data_set in tqdm(util.feature_sets):    
         X_train, X_test, Y_train, Y_test = util.load_movie_data(data_set)
-        model = OneVsRestClassifier(KNeighborsClassifier(), n_jobs = -1)
+        model = OneVsRestClassifier(QuadraticDiscriminantAnalysis(), n_jobs = -1)
         model.fit(X_train, Y_train)
         prediction = model.predict(X_test)
-        filename = data_set + '_output_one_vs_rest_kneighbors.txt'
+        filename = data_set + '_output_one_vs_rest_qda.txt'
         util.output_to_file(data_set, filename, Y_test, prediction)
         
-        util.write_model(model, data_set + '_one_vs_rest_kneighbors')
-    
+        util.write_model(model, data_set + '_one_vs_rest_qda')
+        
 
 
 if  __name__ =='__main__':main()
